554cbd7d9273d7549c26d9b9a4efc9ed674cf6a1
Poornima Nookala <nookala.poornima@gmail.com>
2017-07-02 14:45:50 -0500

    added consolidated tests file

---
+++ QueueBenchmarks/AllTests.sh
@@ -0,0 +1,99 @@
+#! /bin/sh
+
+make clean all
+./sqmain 1 2 SQueueLatencyRaw2 SQueueLatencySummary
+make clean all
+./sqmain 1 4 SQueueLatencyRaw2 SQueueLatencySummary
+./sqmain 1 6 SQueueLatencyRaw2 SQueueLatencySummary
+./sqmain 1 8 SQueueLatencyRaw2 SQueueLatencySummary
+./sqmain 1 12 SQueueLatencyRaw2 SQueueLatencySummary
+./sqmain 1 16 SQueueLatencyRaw2 SQueueLatencySummary
+./sqmain 1 24 SQueueLatencyRaw2 SQueueLatencySummary
+./sqmain 1 32 SQueueLatencyRaw2 SQueueLatencySummary
+./sqmain 1 48 SQueueLatencyRaw2 SQueueLatencySummary
+
+make clean all
+./sqmain 1 96,114,192,228,384 SQueueLatencyRaw2 SQueueLatencySummary
+./sqmain 1 456,768,912,1024 SQueueLatencyRaw2 SQueueLatencySummary
+
+make clean thwithtitle
+./sqmain 1 2 SQueueLatencyRaw2 SQueueThroughputSummary
+make clean th
+./sqmain 1 4,6,8,12,16 SQueueLatencyRaw2 SQueueThroughputSummary
+./sqmain 1 24,32,48 SQueueLatencyRaw2 SQueueThroughputSummary
+./sqmain 1 96,114,192,228,384 SQueueLatencyRaw2 SQueueThroughputSummary
+./sqmain 1 456,768,912,1024 SQueueLatencyRaw2 SQueueThroughputSummary
+
+make clean all
+./sqmain 3 2 BQLatencyRaw2 BQLatencySummary
+make clean all
+./sqmain 3 4 BQLatencyRaw2 BQLatencySummary
+./sqmain 3 6 BQLatencyRaw2 BQLatencySummary
+./sqmain 3 8 BQLatencyRaw2 BQLatencySummary
+./sqmain 3 12 BQLatencyRaw2 BQLatencySummary
+./sqmain 3 16 BQLatencyRaw2 BQLatencySummary
+./sqmain 3 24 BQLatencyRaw2 BQLatencySummary
+./sqmain 3 32 BQLatencyRaw2 BQLatencySummary
+./sqmain 3 48 BQLatencyRaw2 BQLatencySummary
+
+make clean all
+./sqmain 3 96,114,192,228,384 BQLatencyRaw2 BQLatencySummary
+./sqmain 3 456,768,912,1024 BQLatencyRaw2 BQLatencySummary
+
+make clean thwithtitle
+./sqmain 3 2 BQLatencyRaw2 BQThroughputSummary
+make clean th
+./sqmain 3 4,6,8,12,16 BQLatencyRaw2 BQThroughputSummary
+./sqmain 3 24,32,48 BQLatencyRaw2 BQThroughputSummary
+./sqmain 3 96,114,192,228,384 BQLatencyRaw2 BQThroughputSummary
+./sqmain 3 456,768,912,1024 BQLatencyRaw2 BQThroughputSummary
+
+make clean all
+./sqmain 4 2 MSQLatencyRaw2 MSQLatencySummary
+make clean all
+./sqmain 4 4 MSQLatencyRaw2 MSQLatencySummary
+./sqmain 4 6 MSQLatencyRaw2 MSQLatencySummary
+./sqmain 4 8 MSQLatencyRaw2 MSQLatencySummary
+./sqmain 4 12 MSQLatencyRaw2 MSQLatencySummary
+./sqmain 4 16 MSQLatencyRaw2 MSQLatencySummary
+./sqmain 4 24 MSQLatencyRaw2 MSQLatencySummary
+./sqmain 4 32 MSQLatencyRaw2 MSQLatencySummary
+./sqmain 4 48 MSQLatencyRaw2 MSQLatencySummary
+
+make clean all
+./sqmain 4 96,114,192,228,384 MSQLatencyRaw2 MSQLatencySummary
+./sqmain 4 456,768,912,1024 MSQLatencyRaw2 MSQLatencySummary
+
+make clean thwithtitle
+./sqmain 4 2 MSQLatencyRaw2 MSQThroughputSummary
+make clean th
+./sqmain 4 4,6,8,12,16 MSQLatencyRaw2 MSQThroughputSummary
+./sqmain 4 24,32,48 MSQLatencyRaw2 MSQThroughputSummary
+./sqmain 4 96,114,192,228,384 MSQLatencyRaw2 MSQThroughputSummary
+./sqmain 4 456,768,912,1024 MSQLatencyRaw2 MSQThroughputSummary
+
+make clean all
+./sqmain 1 2 RCULatencyRaw2 RCULatencySummary
+make clean all
+./sqmain 1 4 RCULatencyRaw2 RCULatencySummary
+./sqmain 1 6 RCULatencyRaw2 RCULatencySummary
+./sqmain 1 8 RCULatencyRaw2 RCULatencySummary
+./sqmain 1 12 RCULatencyRaw2 RCULatencySummary
+./sqmain 1 16 RCULatencyRaw2 RCULatencySummary
+./sqmain 1 24 RCULatencyRaw2 RCULatencySummary
+./sqmain 1 32 RCULatencyRaw2 RCULatencySummary
+./sqmain 1 48 RCULatencyRaw2 RCULatencySummary
+
+make clean all
+./sqmain 1 96,114,192,228,384 RCULatencyRaw2 RCULatencySummary
+./sqmain 1 456,768,912,1024 RCULatencyRaw2 RCULatencySummary
+
+make clean thwithtitle
+./sqmain 1 2 RCULatencyRaw2 RCUThroughputSummary
+make clean th
+./sqmain 1 4,6,8,12,16 RCULatencyRaw2 RCUThroughputSummary
+./sqmain 1 24,32,48 RCULatencyRaw2 RCUThroughputSummary
+./sqmain 1 96,114,192,228,384 RCULatencyRaw2 RCUThroughputSummary
+./sqmain 1 456,768,912,1024 RCULatencyRaw2 RCUThroughputSummary
+
+
