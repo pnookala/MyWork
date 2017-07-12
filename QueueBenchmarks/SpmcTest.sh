31b8f875b35bb9ae83f5b10e8ddd4a0a41d6638f
Poornima Nookala <nookala.poornima@gmail.com>
2017-07-11 19:58:36 -0500

    changed tests for SPMC

--- QueueBenchmarks/SpmcTest.sh
+++ QueueBenchmarks/SpmcTest.sh
@@ -3,26 +3,26 @@
 make clean all
 ./sqmain 8 1 SPMCLatencyRaw2 SPMCLatencySummary
 make clean all
-./sqmain 8 3 SPMCLatencyRaw2 SPMCLatencySummary
-./sqmain 8 5 LatencyRaw4 SPMCLatencySummary
-./sqmain 8 7 SPMCLatencyRaw6 SPMCLatencySummary
-./sqmain 8 11 SPMCLatencyRaw8 SPMCLatencySummary
-./sqmain 8 15 SPMCLatencyRaw12 SPMCLatencySummary
-./sqmain 8 23 SPMCLatencyRaw16 SPMCLatencySummary
-./sqmain 8 31 SPMCLatencyRaw24 SPMCLatencySummary
-./sqmain 8 47 SPMCLatencyRaw32 SPMCLatencySummary
+./sqmain 8 2 SPMCLatencyRaw2 SPMCLatencySummary
+./sqmain 8 4 LatencyRaw4 SPMCLatencySummary
+./sqmain 8 8 SPMCLatencyRaw6 SPMCLatencySummary
+./sqmain 8 12 SPMCLatencyRaw8 SPMCLatencySummary
+./sqmain 8 16 SPMCLatencyRaw12 SPMCLatencySummary
+./sqmain 8 24 SPMCLatencyRaw16 SPMCLatencySummary
+./sqmain 8 32 SPMCLatencyRaw24 SPMCLatencySummary
+./sqmain 8 48 SPMCLatencyRaw32 SPMCLatencySummary
 
 make clean all
-./sqmain 8 95,113,191,227,383 SPMCLatencyRaw2 SPMCLatencySummary
-./sqmain 8 455,767,911,1023 SPMCLatencyRaw2 SPMCLatencySummary
+./sqmain 8 96,114,192,228,384 SPMCLatencyRaw2 SPMCLatencySummary
+./sqmain 8 456,768,912,1024 SPMCLatencyRaw2 SPMCLatencySummary
 
 make clean thwithtitle
 ./sqmain 8 1 SPMCLatencyRaw2 SPMCThroughputSummary
 make clean th
-./sqmain 8 3,5,7,11,15 SPMCLatencyRaw2 SPMCThroughputSummary
-./sqmain 8 23,31,47 SPMCLatencyRaw2 SPMCThroughputSummary
-./sqmain 8 95,113,191,227,383 SPMCLatencyRaw2 SPMCThroughputSummary
-./sqmain 8 455,767,911,1023 SPMCLatencyRaw2 SPMCThroughputSummary
+./sqmain 8 2,4,8,12,16 SPMCLatencyRaw2 SPMCThroughputSummary
+./sqmain 8 24,32,48 SPMCLatencyRaw2 SPMCThroughputSummary
+./sqmain 8 96,114,192,228,384 SPMCLatencyRaw2 SPMCThroughputSummary
+./sqmain 8 456,768,912,1024 SPMCLatencyRaw2 SPMCThroughputSummary
 
 
 
